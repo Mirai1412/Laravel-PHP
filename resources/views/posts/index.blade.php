dfd4e95cc3fb69170809e3cd52e2cc3ca61b81d2
mirai <kkas0807@naver.com>
2021-07-03 22:54:29 +0900

    CSS post 수정

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -18,73 +18,93 @@
         </style>
  </head>
 <body>
-    <div class="Creat">
-        <h1>Bulletin Board</h1>
-        @auth<!-- 현재로그인한 사용자만 보이게한다.-->
-        <a href="/posts/create">
-            <button type="submit" class="border-2 border-blue-500 rounded-lg font-bold text-blue-500 px-4 py-3 transition duration-300 ease-in-out hover:bg-blue-500 hover:text-white mr-6">Creating a Post</button>
-        </a>
-        @endauth
-        <a href="{{route('dashboard')}}">
-            <button type="submit" class="border-2 border-blue-500 rounded-lg font-bold text-blue-500 px-4 py-3 transition duration-300 ease-in-out hover:bg-blue-500 hover:text-white mr-6"> Dashboard</button>
-        </a>
-    </div>
-    <!-- post card -->
-        @foreach ($posts as $post)
-    <div class="flex2 bg-white shadow-lg rounded-lg mx-4 md:mx-auto my-56 max-w-md md:max-w-2xl "><!--horizantil margin is just for display-->
-    <div class="flex items-start px-4 py-6">
-       <img class="w-12 h-12 rounded-full object-cover mr-4 shadow" src="https://images.unsplash.com/photo-1542156822-6924d1a71ace?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt="avatar">
-       <div class="">
-          <div class="flex items-center justify-between">
-            <span>
-                <a href="{{route('posts.show', $post->id) }}">제목 : {{ $post->title }}</a>
-            </span>
-          </div>
-          <div class="box2">
-            {{  $post->content }}
+    <h1>게시판</h1>
+    <div class="p-16">
+        <div class="grid md:grid-cols-2 sm:grid-cols-1 lg:grid-cols-3 m-5 mb-10">
+
+
+                <div class="bg-white overflow-hidden hover:bg-green-100 border border-gray-200 p-3">
+            <div class="m-2 text-justify text-sm">
+                <p class="text-right text-xs">May 17, 2020</p>
+                <h2 class="font-bold text-lg h-2 mb-8">Blog Post Title </h2>
+                <p class="text-xs">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+
+            </div>
+            <div class="w-full text-right mt-4">
+              <a class="text-green-400 uppercase font-bold text-sm" href="#">Read More</a>
+            </div>
         </div>
-          <div class="mt-4 flex items-center">
-             <div class="flex mr-2 text-gray-700 text-sm mr-3">
-                <svg fill="none" viewBox="0 0 24 24"  class="w-4 h-4 mr-1" stroke="currentColor">
-                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"/>
-                 </svg>
-                <span>12</span>
-             </div>
-             <div class="flex mr-2 text-gray-700 text-sm mr-8">
-                <svg fill="none" viewBox="0 0 24 24" class="w-4 h-4 mr-1" stroke="currentColor">
-                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 8h2a2 2 0 012 2v6a2 2 0 01-2 2h-2v4l-4-4H9a1.994 1.994 0 01-1.414-.586m0 0L11 14h4a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2v4l.586-.586z"/>
-                </svg>
-                <span>8</span>
-             </div>
-             <div class="flex mr-2 text-gray-700 text-sm mr-4">
-                <svg fill="none" viewBox="0 0 24 24" class="w-4 h-4 mr-1" stroke="currentColor">
-                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-8l-4-4m0 0L8 8m4-4v12"/>
-                 </svg>
-                <span>share</span>
-             </div>
+              <div class="bg-white overflow-hidden hover:bg-green-100 border border-gray-200 p-3">
+            <div class="m-2 text-justify text-sm">
+                <p class="text-right text-xs">May 17, 2020</p>
+                <h2 class="font-bold text-lg h-2 mb-8">Blog Post Title </h2>
+                <p class="text-xs">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
 
-          </div>
+            </div>
+            <div class="w-full text-right mt-4">
+              <a class="text-green-400 uppercase font-bold text-sm" href="#">Read More</a>
+            </div>
+        </div>
+              <div class="bg-white overflow-hidden hover:bg-green-100 border border-gray-200 p-3">
+            <div class="m-2 text-justify text-sm">
+                <p class="text-right text-xs">May 17, 2020</p>
+                <h2 class="font-bold text-lg h-2 mb-8">Blog Post Title </h2>
+                <p class="text-xs">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
 
-       </div>
+            </div>
+            <div class="w-full text-right mt-4">
+              <a class="text-green-400 uppercase font-bold text-sm" href="#">Read More</a>
+            </div>
+        </div>
+              <div class="bg-white overflow-hidden hover:bg-green-100 border border-gray-200 p-3">
+            <div class="m-2 text-justify text-sm">
+                <p class="text-right text-xs">May 17, 2020</p>
+                <h2 class="font-bold text-lg h-2 mb-8">Blog Post Title </h2>
+                <p class="text-xs">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
 
-    </div>
+            </div>
+            <div class="w-full text-right mt-4">
+              <a class="text-green-400 uppercase font-bold text-sm" href="#">Read More</a>
+            </div>
+        </div>
+              <div class="bg-white overflow-hidden hover:bg-green-100 border border-gray-200 p-3">
+            <div class="m-2 text-justify text-sm">
+                <p class="text-right text-xs">May 17, 2020</p>
+                <h2 class="font-bold text-lg h-2 mb-8">Blog Post Title </h2>
+                <p class="text-xs">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
 
- </div>
- @endforeach
-    {{-- <h1>게시판</h1>
+            </div>
+            <div class="w-full text-right mt-4">
+              <a class="text-green-400 uppercase font-bold text-sm" href="#">Read More</a>
+            </div>
+        </div>
+              <div class="bg-white overflow-hidden hover:bg-green-100 border border-gray-200 p-3">
+            <div class="m-2 text-justify text-sm">
+                <p class="text-right text-xs">May 17, 2020</p>
+                <h2 class="font-bold text-lg h-2 mb-8">Blog Post Title </h2>
+                <p class="text-xs">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
 
-       <div class="Creat">
-        @auth<!-- 현재로그인한 사용자만 보이게한다.-->
-        <a href="/posts/create">
-            <button type="submit" class="border-2 border-blue-500 rounded-lg font-bold text-blue-500 px-4 py-3 transition duration-300 ease-in-out hover:bg-blue-500 hover:text-white mr-6">Creating a Post</button>
-        </a>
-        @endauth
-        <a href="{{route('dashboard')}}">
-            <button type="submit" class="border-2 border-blue-500 rounded-lg font-bold text-blue-500 px-4 py-3 transition duration-300 ease-in-out hover:bg-blue-500 hover:text-white mr-6"> Dashboard</button>
-        </a>
-    </div>
+            </div>
+            <div class="w-full text-right mt-4">
+              <a class="text-green-400 uppercase font-bold text-sm" href="#">Read More</a>
+            </div>
+        </div>
 
-    <div class="container">
+        </div>
+      </div>
+    {{-- <div class="container">
         <ul>
             @foreach ($posts as $post)
             <li>
@@ -105,6 +125,14 @@
                 </li>
                 @endforeach
             </ul>
+               @auth<!-- 현재로그인한 사용자만 보이게한다.-->
+        <a href="/posts/create">
+            <button type="submit" class="border-2 border-blue-500 rounded-lg font-bold text-blue-500 px-4 py-3 transition duration-300 ease-in-out hover:bg-blue-500 hover:text-white mr-6">Creating a Post</button>
+        </a>
+        @endauth
+        <a href="{{route('dashboard')}}">
+            <button type="submit" class="border-2 border-blue-500 rounded-lg font-bold text-blue-500 px-4 py-3 transition duration-300 ease-in-out hover:bg-blue-500 hover:text-white mr-6"> Dashboard</button>
+        </a>
             <div>
                 {{ $posts->links()}}
             </div>
