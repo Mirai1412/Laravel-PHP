effdfc6a53e8213e140466789635da60c768958c
mirai <kkas0807@naver.com>
2021-07-04 22:26:04 +0900

    CSS POST 추가

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -15,101 +15,58 @@
      h1{
 
      }
+     div.bg{
+         width: 800px;
+         padding: 0;
+         width: 300px;
+     }
+     div.p-16{
+        padding: 0;
+        margin: 0;
+     }
+     div.grid{
+         margin-bottom: 5px;
+     }
         </style>
  </head>
 <body>
     <h1>게시판</h1>
+    @foreach ($posts as $post)
     <div class="p-16">
         <div class="grid md:grid-cols-2 sm:grid-cols-1 lg:grid-cols-3 m-5 mb-10">
-
-
                 <div class="bg-white overflow-hidden hover:bg-green-100 border border-gray-200 p-3">
             <div class="m-2 text-justify text-sm">
-                <p class="text-right text-xs">May 17, 2020</p>
-                <h2 class="font-bold text-lg h-2 mb-8">Blog Post Title </h2>
-                <p class="text-xs">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                </p>
-
-            </div>
-            <div class="w-full text-right mt-4">
-              <a class="text-green-400 uppercase font-bold text-sm" href="#">Read More</a>
-            </div>
-        </div>
-              <div class="bg-white overflow-hidden hover:bg-green-100 border border-gray-200 p-3">
-            <div class="m-2 text-justify text-sm">
-                <p class="text-right text-xs">May 17, 2020</p>
-                <h2 class="font-bold text-lg h-2 mb-8">Blog Post Title </h2>
-                <p class="text-xs">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                </p>
-
-            </div>
-            <div class="w-full text-right mt-4">
-              <a class="text-green-400 uppercase font-bold text-sm" href="#">Read More</a>
+                <p class="text-right text-xs"> {{  $post-> created_at }}</p>
+                <span>
+                    <a href="{{route('posts.show', $post->id) }}">제목 : {{ $post->title }}</a>
+                </span>
+                <div class="box2">
+                    {{  $post->content }}
+                </div>
+                <div class="w-full text-right mt-4">
+                    <a class="text-green-400 uppercase font-bold text-sm" href="#">Read More</a>
+                  </div>
             </div>
         </div>
-              <div class="bg-white overflow-hidden hover:bg-green-100 border border-gray-200 p-3">
-            <div class="m-2 text-justify text-sm">
-                <p class="text-right text-xs">May 17, 2020</p>
-                <h2 class="font-bold text-lg h-2 mb-8">Blog Post Title </h2>
-                <p class="text-xs">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                </p>
-
-            </div>
-            <div class="w-full text-right mt-4">
-              <a class="text-green-400 uppercase font-bold text-sm" href="#">Read More</a>
-            </div>
         </div>
-              <div class="bg-white overflow-hidden hover:bg-green-100 border border-gray-200 p-3">
-            <div class="m-2 text-justify text-sm">
-                <p class="text-right text-xs">May 17, 2020</p>
-                <h2 class="font-bold text-lg h-2 mb-8">Blog Post Title </h2>
-                <p class="text-xs">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                </p>
-
-            </div>
-            <div class="w-full text-right mt-4">
-              <a class="text-green-400 uppercase font-bold text-sm" href="#">Read More</a>
-            </div>
-        </div>
-              <div class="bg-white overflow-hidden hover:bg-green-100 border border-gray-200 p-3">
-            <div class="m-2 text-justify text-sm">
-                <p class="text-right text-xs">May 17, 2020</p>
-                <h2 class="font-bold text-lg h-2 mb-8">Blog Post Title </h2>
-                <p class="text-xs">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                </p>
-
-            </div>
-            <div class="w-full text-right mt-4">
-              <a class="text-green-400 uppercase font-bold text-sm" href="#">Read More</a>
-            </div>
-        </div>
-              <div class="bg-white overflow-hidden hover:bg-green-100 border border-gray-200 p-3">
-            <div class="m-2 text-justify text-sm">
-                <p class="text-right text-xs">May 17, 2020</p>
-                <h2 class="font-bold text-lg h-2 mb-8">Blog Post Title </h2>
-                <p class="text-xs">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                </p>
-
-            </div>
-            <div class="w-full text-right mt-4">
-              <a class="text-green-400 uppercase font-bold text-sm" href="#">Read More</a>
-            </div>
-        </div>
-
-        </div>
-      </div>
+    </div>
+      @endforeach
+      @auth<!-- 현재로그인한 사용자만 보이게한다.-->
+      <a href="/posts/create">
+          <button type="submit" class="border-2 border-blue-500 rounded-lg font-bold text-blue-500 px-4 py-3 transition duration-300 ease-in-out hover:bg-blue-500 hover:text-white mr-6">Creating a Post</button>
+      </a>
+      @endauth
+      <a href="{{route('dashboard')}}">
+          <button type="submit" class="border-2 border-blue-500 rounded-lg font-bold text-blue-500 px-4 py-3 transition duration-300 ease-in-out hover:bg-blue-500 hover:text-white mr-6"> Dashboard</button>
+      </a>
+          <div>
+              {{ $posts->links()}}
+          </div>
     {{-- <div class="container">
         <ul>
             @foreach ($posts as $post)
             <li>
                 <div class="box">
-
 
                     <span>
                         <a href="{{route('posts.show', $post->id) }}">제목 : {{ $post->title }}</a>
